d4ce41f36f65d08d742b6e3b32d9203fdc2357c8
Owenc <hi@achievecreative.com>
2018-06-28 10:06:05 +0800

    Replace the file if exists

--- switch-sitecore-search-provider.ps1
+++ switch-sitecore-search-provider.ps1
@@ -11,23 +11,61 @@ Param([string]$searchProvider)
 Write-Output "Switch search provider to $($searchProvider)."
 
 If ($searchProvider -eq 'azure'){
-    Get-ChildItem -Path .\App_Config\Include\ -Include *solr*.config -Recurse | Rename-Item -NewName {$_.name -Replace '\.config$', '.config.disabled'}
-    Get-ChildItem -Path .\App_Config\Include\ -Include *lucene*.config -Recurse | Rename-Item -NewName {$_.name -Replace '\.config$', '.config.disabled'}
+    Get-ChildItem -Path .\App_Config\Include\ -Include *solr*.config -Recurse | ForEach-Object{
+        $NewName = $_.Name -Replace '\.config$', '.config.disabled'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 
-    Get-ChildItem -Path .\App_Config\Include\ -Include *azure*.config.* -Recurse | Rename-Item -NewName {$_.name -Replace '\.config\..*$', '.config'}
+    Get-ChildItem -Path .\App_Config\Include\ -Include *lucene*.config -Recurse | ForEach-Object{
+        $NewName = $_.Name -Replace '\.config$', '.config.disabled'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
+
+    Get-ChildItem -Path .\App_Config\Include\ -Include *azure*.config.* -Recurse | ForEach-Object{
+        $NewName = $_.Name -Replace '\.config\..*$', '.config'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 }
 
 If($searchProvider -eq 'solr'){
-    Get-ChildItem -Path .\App_Config\Include\ -Include *solr*.config.* -Recurse | Rename-Item -NewName { $_.name -Replace '\.config\..*$', '.config' }
+    Get-ChildItem -Path .\App_Config\Include\ -Include *solr*.config.* -Recurse | ForEach-Object{
+        $NewName = $_.name -Replace '\.config\..*$', '.config'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 
-    Get-ChildItem -Path .\App_Config\Include\ -Include *lucene*.config -Recurse | Rename-Item -NewName { $_.name -Replace '\.config$', '.config.disabled' }
-    Get-ChildItem -Path .\App_Config\Include\ -Include *azure*.config -Recurse | Rename-Item -NewName { $_.name -Replace '\.config$', '.config.disabled' }
+    Get-ChildItem -Path .\App_Config\Include\ -Include *lucene*.config -Recurse | ForEach-Object{
+        $NewName =$_.name -Replace '\.config$', '.config.disabled'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
+
+    Get-ChildItem -Path .\App_Config\Include\ -Include *azure*.config -Recurse | ForEach-Object{
+        $NewName =$_.name -Replace '\.config$', '.config.disabled'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 }
 
 If($searchProvider -eq 'lucene'){
-    Get-ChildItem -Path .\App_Config\Include\ -Include *solr*.config -Recurse | Rename-Item -NewName {$_.name -Replace '\.config$', '.config.disabled'}
+    Get-ChildItem -Path .\App_Config\Include\ -Include *solr*.config -Recurse | ForEach-Object{
+        $NewName =$_.name -Replace '\.config$', '.config.disabled'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 
-    Get-ChildItem -Path .\App_Config\Include\ -Include *lucene*.config.* -Recurse | Rename-Item -NewName {$_.name -Replace '\.config\..*$', '.config'}
+    Get-ChildItem -Path .\App_Config\Include\ -Include *lucene*.config.* -Recurse | ForEach-Object{
+        $NewName = $_.name -Replace '\.config\..*$', '.config'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 
-    Get-ChildItem -Path .\App_Config\Include\ -Include *azure*.config -Recurse | Rename-Item -NewName {$_.name -Replace '\.config$', '.config.disabled'}
+    Get-ChildItem -Path .\App_Config\Include\ -Include *azure*.config -Recurse | ForEach-Object{
+        $NewName =$_.name -Replace '\.config$', '.config.disabled'
+        $Destination = Join-Path -Path $_.Directory.FullName -ChildPath $NewName
+        Move-Item -Path $_.FullName -Destination $Destination -Force
+    }
 }
